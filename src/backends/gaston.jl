e983ab5f324ea6a9c9ca970697de48056623c07a
t-bltg <t-bltg@users.noreply.github.com>
2021-07-25 15:21:23 +0200

    add new gnuplot capabilities (surface, scatter3d, wireframe, ...)

--- src/backends/gaston.jl
+++ src/backends/gaston.jl
@@ -129,45 +129,91 @@ function gaston_add_series(plt::Plot{GastonBackend}, series::Series)
     sp = series[:subplot]
     g_sp = sp.o  # Gaston subplot object
 
-    seriesconf = gaston_parse_series_args(series)  # Gnuplot string
-    c = G.Curve(series[:x], series[:y], nothing, nothing, seriesconf )
+    if series[:seriestype] ∈ (:heatmap, :contour) && g_sp.dims == 2
+        g_sp.dims = 3  # FIXME: this is ugly, we need heatmap to use splot, not plot
+    end
+
+    x = series[:x]
+    y = series[:y]
+    z = g_sp.dims == 2 ? nothing : series[:z]
+    if z isa Surface
+        z = z.surf
+    end
+
+    seriesconf = gaston_seriesconf!(sp, series)  # Gnuplot string
+    c = G.Curve(x, y, z, nothing, seriesconf)
 
     isfirst = length(g_sp.curves) == 0 ? true : false
     push!(g_sp.curves, c)
     G.write_data(c, g_sp.dims, g_sp.datafile, append = isfirst ? false : true)
 end
 
-function gaston_parse_series_args(series::Series)
+gaston_lc_ls_lw(series::Series) = (
+    gaston_color(series[:linecolor], series[:linealpha]),
+    gaston_linestyle(series[:linestyle]),
+    series[:linewidth],
+)
+
+gaston_mk_ms_mc(series::Series) = (
+    gaston_marker(series[:markershape]),
+    series[:markersize] * GASTON_MARKER_SCALING,
+    gaston_color(series[:markercolor], series[:markeralpha]),
+)
+
+gaston_palette(gradient) = begin
+    palette = String[]
+    n = -1
+    for rgba ∈ gradient  # FIXME: naive conversion, inefficient ?
+        push!(palette, "$(n += 1) $(rgba.r) $(rgba.g) $(rgba.b)")
+    end
+    '(' * join(palette, ", ") * ')'
+end
+
+function gaston_seriesconf!(sp, series::Series)
+    gsp = sp.o
     curveconf = String[]
     st = series[:seriestype]
 
-    if st == :scatter
-        pt = gaston_marker(series[:markershape])
-        ps = series[:markersize] * GASTON_MARKER_SCALING
-        lc = gaston_color(series[:markercolor])
-        # alpha = series[:markeralpha] # TODO merge alpha with rgb color
+    clims = get_clims(sp, series)
+    if st ∈ (:scatter, :scatter3d)
+        pt, ps, lc = gaston_mk_ms_mc(series)
         push!(curveconf, """with points pt $pt ps $ps lc $lc""")
-    elseif st == :path
-        lc = gaston_color(series[:linecolor])
-        dt = gaston_linestyle(series[:linestyle])
-        lw = series[:linewidth]
-        # alpha = series[:linealpha] # TODO merge alpha with rgb color
+    elseif st ∈ (:path, :straightline, :path3d)
+        lc, dt, lw = gaston_lc_ls_lw(series)
         if series[:markershape] == :none # simplepath
             push!(curveconf, """with lines lc $lc dt $dt lw $lw""")
         else
-            pt = gaston_marker(series[:markershape])
-            ps = series[:markersize] * GASTON_MARKER_SCALING
+            pt, ps = gaston_mk_ms_mc(series)
             push!(curveconf, """with lp lc $lc dt $dt lw $lw pt $pt ps $ps""")
         end
     elseif st == :shape
-        fc = gaston_color(series[:fillcolor])
+        fc = gaston_color(series[:fillcolor], series[:fillalpha])
         fs = "solid"
-        lc = gaston_color(series[:linecolor])
+        lc, _ = gaston_lc_ls_lw(series)
         push!(curveconf, """with filledcurves fc $fc fs $fs border lc $lc""")
     elseif st == :steppre
         push!(curveconf, """with steps""")
     elseif st == :steppost
         push!(curveconf, """with fsteps""")  # Not sure if not the other way
+    elseif st ∈ (:contour, :contour3d)
+        push!(curveconf, """with lines""")
+        if st == :contour
+            gsp.axesconf *= """\nset view map\nunset surface"""
+        end
+        levels = join(map(string, collect(contour_levels(series, clims))), ", ")
+        gsp.axesconf *= """\nset contour base\nset cntrparam levels discrete $levels """
+    elseif st ∈ (:surface, :heatmap)
+        palette = gaston_palette(series[:seriescolor])
+        gsp.axesconf *= """\nset palette model RGB defined $palette"""
+        if st == :heatmap
+            gsp.axesconf *= """\nset view map"""
+        end
+        push!(curveconf, """with pm3d""")
+    elseif st == :wireframe
+        lc, dt, lw = gaston_lc_ls_lw(series)
+        push!(curveconf, """with lines lc $lc dt $dt lw $lw""")
+    else
+        @warn "Gaston: $st is not implemented yet"
     end
 
     # label
@@ -215,7 +261,14 @@ function gaston_parse_axes_args(plt::Plot{GastonBackend}, sp::Subplot{GastonBack
             ticks = get_ticks(sp, axis_attr)
             gaston_set_ticks!(axesconf, ticks, letter)
         end
-        # set title {"<title-text>"} {offset <offset>} {font "<font>{,<size>}"}{{textcolor | tc} {<colorspec> | default}} {{no}enhanced}1
+
+        ratio = get_aspect_ratio(sp)
+        if ratio != :none
+            if ratio == :equal
+                ratio = -1
+            end
+            push!(axesconf, """set size ratio $ratio""")
+        end
     end
     gaston_set_legend!(axesconf, sp) # Set legend params
 
@@ -317,7 +370,11 @@ function gaston_marker(marker)
     return 1
 end
 
-gaston_color(color) = """rgb "#$(hex(color, :rrggbb))"  """
+gaston_color(color, alpha=0.) = begin
+    col = single_color(color)  # in case of gradients
+    col = alphacolor(col, alpha == nothing ? 0. : alpha)  # add a default alpha if non existent
+    """rgb "#$(hex(col, :aarrggbb))" """
+end
 
 function gaston_linestyle(style)
     style == :solid && return "1"
